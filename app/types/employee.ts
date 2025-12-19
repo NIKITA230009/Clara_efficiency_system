4e8ac5558484b30550c97e75472ad3c7623fa96c
NIKITA230009 <147995617+NIKITA230009@users.noreply.github.com>
2025-12-19 15:47:39 +0300

    new type interface 'employee.ts'

---
+++ app/types/employee.ts
@@ -0,0 +1,10 @@
+export interface Employee {
+    id: string;          // int -> number
+    fullName: string;    // string -> string
+    position: string;
+    basePremium: number; // decimal -> number (в JS всё число)
+    isActive: boolean;   // bool -> boolean
+
+    // Навигационные свойства (массив других интерфейсов)
+    // penalties: Penalty[]; // List<Penalty> -> Penalty[] или Array<Penalty>
+}
